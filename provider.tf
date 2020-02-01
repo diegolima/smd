d68ed95927c90715107c8311c66b9c2048048c07
Diego Lima <diego.lima@adtsys.com.br>
2020-01-31 23:17:10 -0300

    Tweak build

--- provider.tf
+++ provider.tf
@@ -1,8 +1,6 @@
 provider "google-beta" {
-  user_project_override = true
   version               = "~> 3.5"
 }
 provider "google" {
-  user_project_override = true
   version               = "~> 3.5"
 }
