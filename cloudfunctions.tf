05f2b5af18956603e8e3937c0308d13b1614bf28
Diego Lima <diego.lima@adtsys.com.br>
2020-01-31 23:28:57 -0300

    Tweak build

--- cloudfunctions.tf
+++ cloudfunctions.tf
@@ -6,7 +6,7 @@ resource "google_storage_bucket_object" "smd" {
 
 resource "google_cloudfunctions_function" "smd" {
   name        = var.function_name
-  project     = data.google_project.smd.name
+  project     = data.google_project.smd.id
   region      = var.provider_region
   runtime     = var.function_runtime
 
