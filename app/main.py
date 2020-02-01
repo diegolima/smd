cb7766170583e1529fee29bb894f7519b512ef0b
Diego Lima <diego.lima@adtsys.com.br>
2020-02-01 14:44:30 -0300

    Update message

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def hello_world(request):
     try:
         for key in (request.values):
             return key
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World! - Test 6'
+    # request_json = request.get_json()
+    # if request.args and 'message' in request.args:
+    #     return request.args.get('message')
+    # elif request_json and 'message' in request_json:
+    #     return request_json['message']
+    # else:
+    #     return f'Hello World! - Test 6'
